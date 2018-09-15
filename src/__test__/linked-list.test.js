e1d113ca6ab15864728b645f56b6dd8a23af544c
jlhiskey <jasonhi@crazyredhead.com>
2018-09-15 12:11:38 -0700

    fixed testing need to finish readme before final

--- src/__test__/linked-list.test.js
+++ src/__test__/linked-list.test.js
@@ -6,18 +6,21 @@ const testList = new LinkedList();
 const emptyList = new LinkedList();
 
 for (let i = 0; i < 5; i++) {
-  testList.insertAtHead('value $(i)');
+  testList.insertAtHead(`value ${i}`);
 }
 
 describe('#linkedlist.js', () => {
   describe('testing linked-list', () => {
     test('Testing that linked-list.deleteListItem properly removes items', () => {
-      testList.deleteListItem ('value 2');
+      testList.deleteListItem('value 2');
       expect(testList.head.next.next.value).toEqual('value 1');
     });
     test('Testing that linked-list.deleteListItem returns null if no list exists', () => {
-      const output = emptyList.deleteListItem ('value 2');
-      expect(output).toEqual('__ERROR__ The list is empty');
+      try {
+        emptyList.deleteListItem('value 2');
+      } catch (err) {
+        expect(err.message).toEqual('__ERROR__ The list is empty');
+      }
     });
   });
 });
--- src/lib/main.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-const fs = require('fs');
-const bitmapParser = require('./linked-list');
